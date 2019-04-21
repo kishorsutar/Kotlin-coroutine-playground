663dd9e2670796a88f2e00b00c44a7fcc0e3a672
Kishor Sutar <kishor007sutar@gmail.com>
2019-04-22 01:26:49 +0800

    added scope in viewmodel

--- app/src/main/java/com/kishor/kotlin/oroutine/MainVIewModel.kt
+++ app/src/main/java/com/kishor/kotlin/oroutine/MainVIewModel.kt
@@ -1,7 +1,19 @@
 package com.kishor.kotlin.oroutine
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainVIewModel: ViewModel() {
+class MainVIewModel: ViewModel(), CoroutineScope {
 
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    fun callSomething() {
+        val job = launch(coroutineContext) {
+            delay(1000L)
+        }
+    }
 }
