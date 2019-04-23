98b8c867d62925db30fe22f45b2441bb2bf0aa37
Kishor Sutar <kishor007sutar@gmail.com>
2019-04-23 22:53:56 +0800

    Added types of coroutines

--- app/src/main/java/com/kishor/kotlin/oroutine/MainVIewModel.kt
+++ app/src/main/java/com/kishor/kotlin/oroutine/MainVIewModel.kt
@@ -12,8 +12,66 @@ class MainVIewModel: ViewModel(), CoroutineScope {
         get() = Dispatchers.Main + job
 
     fun callSomething() {
-        val job = launch(coroutineContext) {
+        val job = launch(coroutineContext) {// main coroutine scope
             delay(1000L)
         }
     }
+
+
+    // Using different types of coroutine
+    private fun coroutineExample() {
+        launch(Dispatchers.Main) {// new coroutine runs on Android UI thread
+            val result1: Int = withContext(Dispatchers.Default) {// new child context/
+                // do something and that will happen in background thread
+                return@withContext 20
+            }
+
+            println("Result 1 '$result1'")
+
+            val result2Deferred: Deferred<String> = async(Dispatchers.Default) {// new child context
+// do something and that will happen in background thread
+                return@async "Result2"
+            }
+
+            println("result 2 is not available yet")
+
+            val result2: String = result2Deferred.await()
+
+            println("result 2 is '$result2")
+        }
+    }
+
+    // Running coroutine sequentially
+    private fun seq() {
+        launch {
+            val result1: Int = withContext(Dispatchers.Default) {
+                return@withContext 1
+            }
+
+            val result2: Int = withContext(Dispatchers.Default) {
+                println("Result 1 can be used here '$result1")
+                return@withContext 2
+            }
+
+        }
+    }
+
+    // running in parallel
+    private fun par() {
+        launch {
+            val result1Deferred: Deferred<Int> = async(Dispatchers.Default) {
+                // do something
+                return@async 1
+            }
+
+            val result2Deferred: Deferred<Int> = async(Dispatchers.Default) {
+                // do something
+                return@async 2
+            }
+
+            val result1: Int = result1Deferred.await()
+            val result2: Int = result2Deferred.await()
+        }
+    }
+
 }
